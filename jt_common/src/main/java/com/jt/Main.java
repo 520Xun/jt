898c6f7eb1496575b01a2fa514e021cfc3f48754
xun <3296497389@qq.com>
2023-02-27 17:11:45 +0800

    打包三台服务器，使用ngnix管理

---
+++ jt_common/src/main/java/com/jt/Main.java
@@ -0,0 +1,11 @@
+package com.jt; /**
+ * @version: java version 1.8
+ * @Author: xun
+ * @description:
+ * @date: ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
